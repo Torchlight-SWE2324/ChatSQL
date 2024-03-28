217ee7595a48045aa8c86269ce151e13f068d387
Dmitry358 <Dmitry358@github.com>
2024-03-28 06:56:59 +0100

    realizato switch finestre con simulazione authentication

--- ChatSQL/Marco/main.py
+++ ChatSQL/Marco/main.py
@@ -15,38 +15,34 @@ def main():
     if "selected_index" not in st.session_state:
         st.session_state.selected_index = 0 #None????
 
-    #authentication_controller = AuthenticationController()
-    #current_window = authentication_controller.get_current_window_name()
-    #print("main::current_window:",current_window)
+    authentication_controller = AuthenticationController()
+    current_window = authentication_controller.get_current_window_name()
 
     dictionary_container = DictionaryContainer()
-    #loaded_dictionaries = dictionary_container.get_all_dictionaries_names()
-
     select_dictionary_service = SelectDictionaryService(dictionary_container)
     upload_dictionary_service = UploadDictionaryService(dictionary_container)
-
     select_dictionary_controller = SelectDictionaryController(select_dictionary_service)
     upload_dictionary_controller = UploadDictionaryController(upload_dictionary_service)
 
     DictionarySelectionWidget(select_dictionary_controller)
 
-    current_window = "client_window"
     if current_window == "client_window":
         UploadDictionaryWidget(upload_dictionary_controller)
 
-        #if st.sidebar.button("Login", type="primary", on_click=None):
-            #authentication_controller.login()
-            #print("btn login")
+        if st.sidebar.button("Login", type="primary", on_click=None):
+            authentication_controller.login()
+            st.rerun()
+
     elif current_window == "technician_window":
         container_upload = st.sidebar.container()
-         #with container_upload:
-            #if st.sidebar.button("Logout", type="primary", on_click=None):
-                #authentication_controller.logout()
+        with container_upload:
+            if st.sidebar.button("Logout", type="primary", on_click=None):
+                authentication_controller.logout()
+                st.rerun()
 
-    #DictionarySelectionWidget(select_dictionary_controller)
 
-
-    print("----------- FINE MAIN ----------")
+    print("----------- FINE MAIN -----------")
+    print("")
 
 
 
